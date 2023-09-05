d588d707133b7065c9d14e2c1b3ee4d9e8169ba4
Vishal <vishalbhat.com@gmail.com>
2023-09-05 13:07:58 +0530

    Add project files.

---
+++ EnvironmentsExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProcessLevelEnvironmentCoreMVC.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    //constructor
+    public HomeController(IWebHostEnvironment webHostEnvironment)
+    {
+      _webHostEnvironment = webHostEnvironment;
+    }
+
+    [Route("/")]
+    public IActionResult Index()
+    {
+      ViewBag.CurrentEnviornment = _webHostEnvironment.EnvironmentName;
+      return View();
+    }
+  }
+}
